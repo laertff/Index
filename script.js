acf0dda02783bfe45fe4a3e6aed9155d1d339bd9
laertff <laert.ff3@gmail.com>
2021-11-29 00:30:22 -0300

    Projeto Git

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-const Buttons = () => {
+const Button = () => {
     const button = document.querySelector('#branco');
     
     
     button.addEventListener('click', () => {
         const body = document.querySelector('body')
-        body.classList.remove('bodyBlack')
         body.classList.toggle('bodyWhite')
         
     })
 };
 
-Buttons();
+Button();
